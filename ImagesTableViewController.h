b5da0b92cbc6ea81841a058d893a4f7f3973546e
Carlos Gomez <jcadrg@gmail.com>
2015-06-25 15:56:45 -0400

    Table view controller implemented

---
+++ ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Mac on 6/24/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
---
+++ ImagesTableViewController.m
@@ -0,0 +1,142 @@
+//
+//  ImagesTableViewController.m
+//  Blocstagram
+//
+//  Created by Mac on 6/24/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import "ImagesTableViewController.h"
+
+@interface ImagesTableViewController ()
+
+@end
+
+@implementation ImagesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    for (int i = 1; i<=10; i++) {
+        NSString *imageName =[NSString stringWithFormat:@"%d.jpg",i];
+        UIImage *image =[UIImage imageNamed:imageName];
+        if (image) {
+            [self.images addObject:image];
+        }
+    }
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.images.count;
+}
+
+-(id) initWithStyle:(UITableViewStyle)style{
+    self = [super initWithStyle:style];
+    if (self) {
+        //custom initialization
+        self.images =[NSMutableArray array];
+    }
+    
+    return self;
+}
+
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
+    
+    static NSInteger imageViewTag = 1234;
+    UIImageView *imageView = (UIImageView *)[cell.contentView viewWithTag:imageViewTag];
+    
+    if(!imageView){
+        //cell with no image yet!!
+        imageView =[[UIImageView alloc] init];
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        
+        imageView.frame = cell.contentView.bounds;
+        imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        
+        imageView.tag = imageViewTag;
+        [cell.contentView addSubview:imageView];
+        
+        
+    }
+    
+    UIImage * image = self.images[indexPath.row];
+    imageView.image = image;
+ 
+    
+    return cell;
+}
+
+-(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIImage *image =self.images[indexPath.row];
+    return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
+}
+
+
+// Override to support conditional editing of the table view.
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+
+
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [self.images removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    
+        } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
